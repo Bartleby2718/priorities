1995781376e35aff6125dc6b6b6257273d5679e0
Jihoon Park <bartleby2718@gmail.com>
2020-04-25 13:49:57 -0400

    Finish assignment/unassignment (#4)

--- connectdb.php
+++ connectdb.php
@@ -14,20 +14,16 @@ $dbname = 'priorities';
 $dsn = "mysql:host=$host;dbname=$dbname";
 
 /** connect to the database **/
-try 
-{
-   $db = new PDO($dsn, $username, $password);   
-   echo "<p>You are connected to the database</p>";
-}
-catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
+try {
+   $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
 {
    // Call a method from any object, 
    // use the object's name followed by -> and then method's name
    // All exception objects provide a getMessage() method that returns the error message 
-   $error_message = $e->getMessage();        
+   $error_message = $e->getMessage();
    echo "<p>An error occurred while connecting to the database: $error_message </p>";
-}
-catch (Exception $e)       // handle any type of exception
+} catch (Exception $e)       // handle any type of exception
 {
    $error_message = $e->getMessage();
    echo "<p>Error message: $error_message </p>";
@@ -35,7 +31,7 @@ catch (Exception $e)       // handle any type of exception
 
 ?>
 
-<?php 
+<?php
 // To close a connection, uncomment the following line
 // $db = null;
 ?>
