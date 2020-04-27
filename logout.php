6a6a987115e31369698fa8b4d4fcf56c600b606b
miashaker <mes2hu@virginia.edu>
2020-04-26 21:15:52 -0400

    last touch fix of profile

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
 <?php
 setcookie('email', "", time() - 3600);
+setcookie("list_ID", "", time() - 3600);
+setcookie("group_ID", "", time() - 3600);
+setcookie("workspace_name", "", time() - 3600);
+
+session_unset();
+session_destroy();
 
 header("Location: login.php");
 exit();
